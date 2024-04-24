00efa3277b83f6e3fda8763f6105f636367b3f09
bhavikgoyal <bhavikgoyal@gmail.com>
2024-04-24 22:42:38 +0530

    Created API's with swagger as required in react.

---
+++ ReactDemo/Controllers/HomeBannerController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestDemo.Service;
+
+namespace TestDemo.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class HomeBannerController : ControllerBase
+	{
+		private readonly ITenantService _tenantService;
+
+		public HomeBannerController(ITenantService tenantService)
+		{
+			_tenantService = tenantService;
+		}
+
+		[HttpGet]
+		public IActionResult GetHomeBanner()
+		{
+			// Use the static host "bar" to get the tenant for the HomeBannerController
+			var tenant = _tenantService.GetTenantByHost("bar");
+			if (tenant == null)
+			{
+				return NotFound("Tenant not found for the provided host.");
+			}
+
+			return Ok(tenant);
+		}
+	}
+}
