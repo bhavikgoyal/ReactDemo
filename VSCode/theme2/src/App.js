6a3e25424bf8eac00ec92365c17b231249c705a7
bhavikgoyal <bhavikgoyal@gmail.com>
2024-04-26 15:37:10 +0530

    Create seperate projects for theme

---
+++ VSCode/theme2/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ThemeSwitcher from './ThemeSwitcher';
+import './App.css'
+
+function App() {
+  return (
+    <ThemeSwitcher />
+  );
+}
+
+export default App;
---
+++ VSCode/theme2/src/FileUploader.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './Themetwo.css'
+
+const FileUploader = ({ onFileChange }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+    onFileChange(file);
+  };
+
+  return (
+    <div>
+      <input type="file" className='navFile form-control '  onChange={handleFileChange} id='first'/>
+    </div>
+  );
+};
+
+export default FileUploader;
